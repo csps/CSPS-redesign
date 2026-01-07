dd50951621c3e100ebb85abf0e352ffdfa4fc65d
roginand <roginandv@gmail.com>
2026-01-07 12:38:34 +0800

    synthesized the incoming changes and the current changes to resolve conflict

--- frontend/src/router/AppRoutes.tsx
+++ frontend/src/router/AppRoutes.tsx
@@ -21,12 +21,10 @@ import AdminEventPage from "../pages/admin/event/page";
 import AdminFinancePage from "../pages/admin/dashboard/finance";
 import AdminProductsPage from "../pages/admin/products";
 import AdminSalesPage from "../pages/admin/sales";
-<<<<<<< HEAD
-=======
+
 import AdminForumPage from "../pages/admin/forum";
 import AdminMerchPage from "../pages/admin/merch";
-import AdminMercheOrdersPage from '../pages/admin/merch/orders';
->>>>>>> origin/main
+import AdminMercheOrdersPage from "../pages/admin/merch/orders";
 
 import NotFoundPage from "../pages/notFound";
 import LoadingPage from "../pages/loading";
@@ -37,7 +35,6 @@ const routers = [
   { path: "/login", element: <LoginPage /> },
   { path: "/contact-us", element: <ContactUsPage /> },
   { path: "/forgot-password", element: <ForgotPasswordPage /> },
-<<<<<<< HEAD
 
   // Student Protected Routes
   {
@@ -64,29 +61,13 @@ const routers = [
       { path: "/admin/dashboard/finance", element: <AdminFinancePage /> },
       { path: "/admin/products", element: <AdminProductsPage /> },
       { path: "/admin/sales", element: <AdminSalesPage /> },
+      { path: "/admin/forum", element: <AdminForumPage /> },
+      { path: "/admin/merch", element: <AdminMerchPage /> },
+      { path: "/admin/merch/orders", element: <AdminMercheOrdersPage /> },
     ],
   },
 
   // 404 fallback
-=======
-  { path: "/dashboard", element: <DashboardPage /> },
-  { path: "/resources", element: <ResourcesPage /> },
-  { path: "/events", element: <EventsPage /> },
-  { path: "/events/view/:id", element: <EventViewPage /> },
-  { path: "/merch", element: <MerchPage /> },
-  { path: "/merch/product", element: <ProductViewPage /> },
-  { path: "/merch/transactions", element: <TransactionsPage /> },
-  { path: "/merch/cart", element: <CartPage /> },
-  { path: "/bulletin", element: <BulletinPage /> },
-  { path: "/admin/dashboard", element: <AdminDashboardPage /> },
-  { path: "/admin/event", element: <AdminEventPage /> },
-  { path: "/admin/dashboard/finance", element: <AdminFinancePage /> },
-  { path: "/admin/products", element: <AdminProductsPage /> },
-  { path: "/admin/sales", element: <AdminSalesPage /> },
-  { path: "/admin/forum", element: <AdminForumPage /> },
-  { path: "/admin/merch", element: <AdminMerchPage /> },
-  { path: "/admin/merch/orders", element: <AdminMercheOrdersPage />},
->>>>>>> origin/main
   { path: "*", element: <NotFoundPage /> },
 ];
 
