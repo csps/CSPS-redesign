fdd84f6808d560705e339f4c9ad6e362cae30378
roginand <roginandv@gmail.com>
2026-01-12 17:34:08 +0800

    deleted changed files

--- frontend/src/enums/PurchaseItemStatus.ts
+++
@@ -1,8 +0,0 @@
-export const PurchaseItemStatus = {
-  TO_BE_CLAIMED: "TO_BE_CLAIMED",
-  NOT_PAID: "NOT_PAID",
-  CLAIMED: "CLAIMED",
-} as const;
-
-export type PurchaseItemStatus =
-  (typeof PurchaseItemStatus)[keyof typeof PurchaseItemStatus];
